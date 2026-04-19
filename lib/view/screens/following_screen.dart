aa3d4a44fb397b866e9cdc42d113c8635a2c3ce0
SJ_b <sjbasak@outlook.com>
2026-04-19 21:27:19 +0530

    feat: user input widgets

--- lib/view/screens/following_screen.dart
+++ lib/view/screens/following_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 
-class FollowingScreen extends StatelessWidget {
+class FollowingScreen extends StatefulWidget {
   const FollowingScreen({super.key});
 
   @override
+  State<FollowingScreen> createState() => _FollowingScreenState();
+}
+
+class _FollowingScreenState extends State<FollowingScreen> {
+  double engagement = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Text("FollowingScreen");
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Center(
+        child: Column(
+          mainAxisSize: .min,
+          children: [
+            Slider(
+              divisions: 10,
+              value: engagement,
+              onChanged: (value) {
+                setState(() {
+                  engagement = value;
+                });
+              },
+            ),
+            Text("Increase Engagement > ${((engagement*100).round() / 100)*100}%"),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/view/screens/profile_screen.dart
+++ lib/view/screens/profile_screen.dart
@@ -11,46 +11,83 @@ class _ProfileScreenState extends State<ProfileScreen> {
   TextEditingController userID = TextEditingController();
   TextEditingController password = TextEditingController();
 
+  bool? isGuestMode = false;
+  bool rememberMe = false;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(12),
-      child: Center(
-        child: Column(
-          mainAxisSize: .min,
-          spacing: 12,
-          children: [
-            Icon(Icons.person_rounded, size: 178),
-            TextField(
-              controller: userID,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.person),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(22)),
-                ),
+      child: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisSize: .min,
+            spacing: 12,
+            children: [
+              InkWell(
+                borderRadius: BorderRadius.circular(40),
+                splashColor: Colors.cyan,
+                onTap: () {
+                  print("Tapped the well");
+                },
+                child: Icon(Icons.person_rounded, size: 178),
               ),
-              onEditingComplete: () {
-                setState(() {});
-                FocusManager.instance.primaryFocus?.unfocus();
-              },
-            ),
-            TextField(
-              controller: password,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.key),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(22)),
+              TextField(
+                controller: userID,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(22)),
+                  ),
                 ),
+                onEditingComplete: () {
+                  setState(() {});
+                  FocusManager.instance.primaryFocus?.unfocus();
+                },
               ),
-              onEditingComplete: () {
-                setState(() {});
+              TextField(
+                controller: password,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.key),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(22)),
+                  ),
+                ),
+                onEditingComplete: () {
+                  setState(() {});
 
-                FocusManager.instance.primaryFocus?.unfocus();
-              },
-            ),
-            Text("User ID: ${userID.text}"),
-            Text("Password ID: ${password.text}"),
-          ],
+                  FocusManager.instance.primaryFocus?.unfocus();
+                },
+              ),
+              DropdownButtonFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.settings),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(22)),
+                  ),
+                ),
+                items: [
+                  DropdownMenuItem(value: "SM", child: Text("Social Media")),
+                  DropdownMenuItem(value: "MP", child: Text("Marketplace")),
+                ],
+                onChanged: (value) {
+                  print(value);
+                },
+              ),
+              SwitchListTile(
+                title: const Text("Remember me"),
+                value: rememberMe,
+                onChanged: (value) {
+                  setState(() {
+                    rememberMe = value;
+                  });
+                },
+              ),
+              FilledButton(onPressed: (){
+
+              }, child: const Text("Log In"))
+            ],
+          ),
         ),
       ),
     );
